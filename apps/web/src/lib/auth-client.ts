c2bba5335addc663aec14b77fdfd23e99e35dcdf
xeisenberg <94274140+xxeisenberg@users.noreply.github.com>
2026-09-13 22:34:55 +0530

    use environment variable for auth client baseURL

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,6 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    // baseURL: "https://x44.xxeisenberg.workers.dev"
-    baseURL: "http://localhost:8787"
-})
+  baseURL: process.env.NEXT_PUBLIC_CONTROL_PANEL_URL,
+});
